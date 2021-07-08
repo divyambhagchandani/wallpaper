7dedb5950bed2c85375da4e5ec8bf728ec54190c
Divyam Bhagchandani <divyam17339@iiitd.ac.in>
2021-07-08 21:23:51 +0530

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -22,7 +22,7 @@ class SearchBar extends React.Component {
             <a href="https://wallpapersio.herokuapp.com/" style={{color:"black"}}><label style={hstyle}>Wallpapers.io</label></a>
             <br/><br/>
             <div style={{fontFamily:"monospace", fontSize:"1.2em"}}>
-            <a href="/#" onClick={e => { this.onClickSugg(e,'Nature')}}> Nature</a> <a href="/#" onClick={e => { this.onClickSugg(e,'Sea')}}>Sea</a> <a href="/#" onClick={e => { this.onClickSugg(e,'Travel')}}>Travel</a> <a href="/#" onClick={e => { this.onClickSugg(e,'Mountains')}}>Mountains</a> <a href="/#" onClick={e => { this.onClickSugg(e,'Architecture')}}>Architecture</a> <a href="/#" onClick={e => { this.onClickSugg(e,'Experimental')}}>Experimental</a> <a href="/#"onClick={e => { this.onClickSugg(e,'Interiors')}}>Interiors</a> <a href="/#"onClick={e => { this.onClickSugg(e,'LGBT')}} className="rainbowText">LGBTQ+</a> 
+            <a href="/#" onClick={e => { this.onClickSugg(e,'Nature')}}> Nature</a> <a href="/#" onClick={e => { this.onClickSugg(e,'Sea')}}>Sea</a> <a href="/#" onClick={e => { this.onClickSugg(e,'Travel')}}>Travel</a> <a href="/#" onClick={e => { this.onClickSugg(e,'Mountains')}}>Mountains</a> <a href="/#" onClick={e => { this.onClickSugg(e,'Architecture')}}>Architecture</a> <a href="/#" onClick={e => { this.onClickSugg(e,'Experimental')}}>Experimental</a> <a href="/#"onClick={e => { this.onClickSugg(e,'Interiors')}}>Interiors</a> <a href="/#"onClick={e => { this.onClickSugg(e,'LGBT')}} className="rainbowText">Be gay do crime</a> 
             </div>
             <input
               type="text"
